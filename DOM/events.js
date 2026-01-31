21aa1c5c203af25baf54da065a3146f7c8f18459
Yash Rajput <yashrajput@Yashs-MacBook-Air-3.local>
2026-01-31 22:33:00 +0530

    Add events

---
+++ DOM/events.js
@@ -0,0 +1,15 @@
+/* events in javascript */
+/*What are events in javascript?
+Events are actions or occurrences that happen in the browser,
+such as a user clicking a button, typing in an input field, or scrolling the page.
+Events are used to trigger actions or responses in the browser.
+
+*/
+
+/*Types of events in javascript
+1. Mouse events
+2. Keyboard events
+3. Form events
+4. Document events
+5. Window events
+*/  
